6e3dff9a76ae581b8e9b19b013b828fe65c18337
przlaicu <41859496+przlaicu@users.noreply.github.com>
2024-05-26 19:03:24 +0200

    26.05.2024 rješenje domaće zadaće

--- JavaScript/Code/09 - Arrays/Homework/script.js
+++ JavaScript/Code/09 - Arrays/Homework/script.js
@@ -1,1 +1,19 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const parni = [];
+var number = 0;
+var calculated = 0;
+var average = 0;
+
+for (var i = 0; i <= numbers.length; i++) {
+    number = numbers[i];
+    if (number % 2 == 0) {
+        parni.push(number);
+        calculated = number + calculated;
+        average = calculated / parni.length;
+    }
+
+}
+
+console.log('Parni brojevi u listi su: ' + parni);
+console.log('Zbroj parnih brojeva u array-u je: ' + calculated);
+console.log('The average of even number is: ' + average);
